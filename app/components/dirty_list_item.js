cf15308cc7a789c024b4d06e44183f93a2e44b61
Jorybraun <braunjory@gmail.com>
2016-09-09 14:45:03 -0400

    it adds class to list item

--- app/components/dirty_list_item.js
+++ app/components/dirty_list_item.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { Componenet } from 'react';
 
-const DirtyListItem = (props) => {
-  return <li></li>
+
+
+class DirtyListItem extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      colors: props.dirtyItem.colors,
+      sizes: props.dirtyItem.sizes
+    } 
+    
+    this.addColors = this.addColors.bind(this);
+    this.addSizes = this.addSizes.bind(this);
+  }
+
+  addColors (color) {
+    let colors = this.state.colors;
+    colors.push(color);
+    this.setState({colors: colors});
+  } 
+
+  addSizes (size) {
+    let sizes = this.state.sizes;
+    sizes.push(size);
+    this.setState({sizes: sizes});
+  }
+
+  render() {
+    return (
+      <li>
+      
+      </li>
+    )
+  }
 };
 
-export default DirtyListItem
+export default DirtyListItem;
